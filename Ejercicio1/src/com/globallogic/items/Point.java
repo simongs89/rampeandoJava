8b9c7daf2b60524f6f939f0500cf1aa03149ae07
Simon Gomez <simon.gomez@globallogic.com>
2016-04-29 16:09:18 -0300

    bla

--- Ejercicio1/src/com/globallogic/items/Point.java
+++ Ejercicio1/src/com/globallogic/items/Point.java
@@ -2,7 +2,6 @@ package com.globallogic.items;
 
 import lombok.Data;
 
-//TODO ver lombok (opcional)
 public @Data class Point {
 	private int x;
 	private int y;
@@ -19,6 +18,22 @@ public @Data class Point {
 		this.y = y.intValue();
 	}
 	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
 	public void multiply(int k){
 		this.x *= k;
 		this.y *= k;
