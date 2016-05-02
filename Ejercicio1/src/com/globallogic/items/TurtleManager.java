a36b24613fa0a1da1fa192a555aba837272ef76d
Fernando Farias <yiowns@gmail.com>
2016-05-02 16:50:57 +0200

    Comentarios a TurtleManager

--- Ejercicio1/src/com/globallogic/items/TurtleManager.java
+++ Ejercicio1/src/com/globallogic/items/TurtleManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class TurtleManager {
 
+	//TODO final ?
 	public int solution(int[] array) {
 
 		Turtle turtle = new Turtle(new Point(0, 0));
@@ -14,7 +15,8 @@ public class TurtleManager {
 			turtle.rotate(90);
 
 			ArrayList<Segment> path = turtle.giveMeYourPath();
-
+			
+			//TODO ahorrarse este if
 			if (path.size() > 1) {
 				Segment lastSegment = path.get(path.size() - 1);
 				for (Segment segment : path) {
